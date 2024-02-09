6ef5233fd14a9e70060660fac9c1911fc3bafbea
SonJM <blisle@naver.com>
2024-02-09 23:21:38 +0900

    [HOTFIX] 급합니다.

---
+++ front-end/src/components/SignInComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-actions>
+        <v-btn color="primary" @click.stop="dialog = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    dialog: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
