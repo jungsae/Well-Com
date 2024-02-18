adec4b385fce8ee2f59264715b5b4d4441c073cb
Saehan <wjdtpgks388@gmail.com>
2024-02-19 02:49:29 +0900

    [ADD] Main Footer

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,20 +2,24 @@
   <v-app>
     <Header v-if="isMain"></Header>
     <router-view />
+    <Footer v-if="isMain"></Footer>
   </v-app>
 </template>
 
 <script>
 import Header from "./components/MainHeader.vue";
+import Footer from "./components/MainFooter.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 
   data: () => ({
     showHeader: true,
+    showFooter: true,
     isMain: false,
   }),
   computed: {
