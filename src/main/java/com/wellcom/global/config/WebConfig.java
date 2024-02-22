88dfa9f846080cb98d229e9a237ddbbeeb51403f
SonJM <blisle@naver.com>
2024-02-22 21:49:47 +0900

    [FIX] 회원 가입 중복 이메일 처리

--- src/main/java/com/wellcom/global/config/WebConfig.java
+++ src/main/java/com/wellcom/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 적용
-                .allowedOrigins("http://localhost:8081") // 허용할 오리진 지정
+                .allowedOrigins("http://localhost:8081","http://192.168.0.12:8081") // 허용할 오리진 지정
                 .allowedMethods("*") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true) // 쿠키 허용
